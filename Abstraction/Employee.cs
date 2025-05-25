98ce04039d77ed7cf859f8e2b5aa853d54af7d7e
BURAK DÜLGEROĞLU <dulgerogluburak04@gmail.com>
2025-05-25 19:48:37 +0700

    Abstraction

---
+++ Abstraction/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Abstraction
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Department { get; set; }
+       
+        public abstract void Gorev();
+    }
+
+    public class ProjectManager : Employee
+    { 
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"Ben {Name} {Surname}, proje yöneticisi olarak çalışıyorum.");
+        }
+    }
+}
+
---
+++ Abstraction/Program.cs
@@ -0,0 +1,15 @@
+﻿using Abstraction;
+
+class Program
+{
+    public static void Main()
+    {
+        ProjectManager projectManager = new ProjectManager();
+
+        projectManager.Name = "Hasan";
+        projectManager.Surname = "Çıldırmış";
+
+        projectManager.Gorev();
+
+    }
+}
